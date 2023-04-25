cb7263199e7434257610b8852d7f3bc449c306da
Shaywhytee <swensen42@gmail.com>
2023-04-25 14:18:27 -0600

    first upload

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Align from "./align"
+import Color from "./color_me"
+import Counter from "./counter"
+import Hide from "./hide_me"
+import Text from "./text"
+import Time from "./time"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
+
